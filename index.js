840126a968462d55c2db6c447ba2e50a0e229311
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-09 17:39:13 +0600

    create login api and jwt and send client

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,17 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Explore JWT Token!')
+})
+
+app.post('/login', (req, res) => {
+    const user = req.body;
+    if (user.email === 'mdmehedihasan384@gmail.com' && user.password === '123456') {
+        const token = jwt.sign({ email: user.email }, process.env.ACCESS_JWT_SECRET);
+        res.send({ token })
+    }
+    else {
+        res.send({ success: false })
+    }
 })
 
 app.listen(port, () => {
